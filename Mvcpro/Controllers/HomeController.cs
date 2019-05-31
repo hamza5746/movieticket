f32d8aa86df6fcca272973872e241d83a9a927e4
Hamza <hamza_hanif77@hotmail.com>
2019-05-31 17:34:48 +0500

    Final Commit

---
+++ Mvcpro/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Mvcpro.Models;
+using Mvcpro.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvcpro.Controllers
+{
+    public class HomeController : Controller
+    {
+        cinema_appContext db = new cinema_appContext();
+      
+        // GET: Home
+        public ActionResult Index(String date)
+        {
+           
+
+        // GET: Home
+            vmCinema vmCi = new vmCinema();
+
+            if (date != null)
+            {
+                ViewBag.date = date;
+                DateTime da= DateTime.Parse(date); 
+                vmCi.Shows = db.shows.Where(s => s.show_date == da).ToList();
+                vmCi.Cinemas = db.cinemas.ToList();
+                vmCi.Movies = db.movies.ToList();
+                
+            }
+            return View(vmCi);
+        }
+
+        public ActionResult bktickets(int? id)
+        {
+            vmCinema vmCi = new vmCinema();
+            vmCi.showinfo = db.shows.Where(s => s.show_id == id).Single();
+            vmCi.ticketinfo = db.Tickets.Where(t => t.show_id == id).ToList();
+            //if (price != null)
+            //{
+            //    int totalprice = 0;
+            //    for (int i = 0; i < price.Length; i++)
+            //    {
+            //        totalprice += int.Parse(price[i]);
+            //    }
+            //    vmCi.totalprice = totalprice;
+            //}
+           
+            return View(vmCi);
+        }
+
+
+        [HttpPost,ActionName("bktickets")]
+        public ActionResult submit1(int? showid, String[] seatno1) {
+            vmCinema vmCi = new vmCinema();
+
+            
+            if (seatno1 != null && showid!=0)
+            {
+                vmCi.showinfo = db.shows.Where(s => s.show_id == showid).Single();
+                int count = db.Tickets.Count();
+                count++;
+                for (int i = 0; i < seatno1.Length; i++)
+                {
+                    if (ModelState.IsValid)
+                    {
+                        db.Tickets.Add(new Ticket
+                        {
+                            ticket_id= count,
+                            show_id = (int)showid,
+                            ticket_price = 700,
+                            seatno = seatno1[i]
+                        });
+                        db.SaveChanges();
+                        count++;
+                    }
+                }
+                
+            }
+            
+            return RedirectToAction("bktickets",new {id=showid} );
+
+        }
+    }
+}
---
+++ Mvcpro/ViewModel/vmCinema.cs
@@ -0,0 +1,25 @@
+﻿using Mvcpro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvcpro.ViewModel
+{
+    public class vmCinema
+    {
+        public IEnumerable<show> Shows { get; set; }
+        public IEnumerable<cinema> Cinemas { get; set; }
+
+        public IEnumerable<movy> Movies { get; set; }
+
+        public show showinfo { get; set; }
+
+        public IEnumerable<String> seatno { get; set; }
+
+        public int totalprice {get; set; }
+
+        public IEnumerable<Ticket> ticketinfo { get; set; }
+
+    }
+}
